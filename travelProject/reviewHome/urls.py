f241b6616c5c5175332c01536afef95988f040c6
seyeon <se020122@naver.com>
2026-03-31 14:26:13 +0900

    3_27 과제: CRUD 기능 추가

--- travelProject/reviewHome/urls.py
+++ travelProject/reviewHome/urls.py
@@ -4,4 +4,8 @@ from reviewHome import views
 
 urlpatterns = [
     path("", views.reviewHome, name="reviewHome"),
+    path("blog_list/", views.blog_list, name="blog_list"),
+    path("blog_detail/<int:blog_id>/", views.blog_detail, name="blog_detail"),
+    path("blog_update/<int:id>/", views.blog_update, name="blog_update"),
+    path("blog_delete/<int:id>/", views.blog_delete, name="blog_delete"),
 ]
--- travelProject/reviewHome/views.py
+++ travelProject/reviewHome/views.py
@@ -1,5 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404 
+from travel.models import Review
+from travel.forms import ReviewModelForm
 
 # Create your views here.
 def reviewHome(request):
     return render(request, "blog-home.html")
+
+def blog_list(request):
+    review = Review.objects.all().order_by('-created_at')
+    return render(request, "blog_list.html", {"review" : review})
+
+
+def blog_detail(request, blog_id):
+    blog = get_object_or_404(Review, pk=blog_id)
+    return render(request, "blog_detail.html", {"review": blog})
+
+
+def blog_update(request, id):
+    review = get_object_or_404(Review, pk=id) 
+
+    if request.method == 'POST':
+        form = ReviewModelForm(request.POST, instance=review) 
+        if form.is_valid():
+            form.save()
+            return redirect('blog_detail', blog_id=review.id) 
+    else:
+        form = ReviewModelForm(instance=review)
+    return render(request, 'form_create.html', {'form': form, 'blog_id': id})
+
+
+def blog_delete(request, id):
+    blog = Review.objects.get(pk=id)
+    blog.delete()
+    return redirect('blog_list')
