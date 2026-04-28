0bbf4463900da0a49eb6d4726293127c834fa391
seyeon <se020122@naver.com>
2026-04-28 10:48:12 +0900

    최종: 글 수정/삭제 권한 기능

--- travelProject/travel/models.py
+++ travelProject/travel/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Review(models.Model):
@@ -6,6 +7,8 @@ class Review(models.Model):
     body = models.TextField(verbose_name="내용", default="")
     created_at = models.DateTimeField(verbose_name="작성일", auto_now_add=True)
     photo = models.ImageField(verbose_name="이미지", blank=True, null=True, upload_to='blog_photo')
+
+    author = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
 
     def __str__(self):  #admin에서 글 제목을 표시
         return self.title
